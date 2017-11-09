4b65f9202d8a10db14b8a1676be1fa82bcc490f1
Unknown <alesya_lobatch@mail.ru>
2017-11-09 19:39:43 +0300

    Orders: Added fixes to the server files
    refs: HTDT-279

--- node-server/routes/orders.js
+++ node-server/routes/orders.js
@@ -1,9 +1,22 @@
-module.exports = ((app, fs) => {
-    app.get('/api/orders', (req, res) => {
-        res.status(200).send(fs.readFileSync('data.json', 'utf8'));
-    });
+const express = require('express');
+const router = express.Router();
+const fs = require('file-system');
 
-    app.post('/api/orders', (req, res) => {
-        console.log('Works');
-    });
+router.get('/api/orders', (req, res) => {
+    res.status(200).send(fs.readFileSync('data.json', 'utf8'));
 });
+
+router.post('/api/orders', (req, res) => {
+    const data = JSON.parse(fs.readFileSync('data.json', 'utf8')).orders;
+    const order = req.body.order;
+
+    order.id = data.length + 1;
+    order.dateAndTime = new Date().getTime();
+    order.deliveryState = 'Ожидает доставки';
+
+    data.push(order);
+    fs.writeFileSync('data.json', JSON.stringify({orders : data}));
+    res.status(200).send({"order" : order});
+});
+
+module.exports = router;
--- node-server/server.js
+++ node-server/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const fs = require('file-system');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(morgan('common'));
 
-require('./routes/orders.js')(app, fs);
+app.use(require('./routes/orders.js'));
 
 app.listen(9999, function () {
     console.log('Server has been started...');
