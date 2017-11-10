0eeb316762614600dffce2e14433d269128ca930
Unknown <alesya_lobatch@mail.ru>
2017-11-10 13:23:40 +0300

    Orders: Moved commodities data to the server
    refs: HTDT-279

--- orders-table/app/components/add-edit-form.js
+++ orders-table/app/components/add-edit-form.js
@@ -1,71 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  commodities: [{
-    id: 1,
-    name: 'Барабаны',
-    price: 600
-  }, {
-    id: 2,
-    name: 'Гитара',
-    price: 300
-  }, {
-    id: 3,
-    name: 'Басс-гитара',
-    price: 250
-  }, {
-    id: 4,
-    name: 'Синтезатор',
-    price: 350
-  }, {
-    id: 5,
-    name: 'Чехол',
-    price: 50
-  }, {
-    id: 6,
-    name: 'Тюнер',
-    price: 35
-  }, {
-    id: 7,
-    name: 'Барабанные палочки',
-    price: 15
-  }],
-  districts: [{
-    id: 1,
-    name: 'Центральный'
-  }, {
-    id: 2,
-    name: 'Советский'
-  }, {
-    id: 3,
-    name: 'Первомайский'
-  }, {
-    id: 4,
-    name: 'Партизанский'
-  }, {
-    id: 5,
-    name: 'Заводской'
-  }, {
-    id: 6,
-    name: 'Ленинский'
-  }, {
-    id: 7,
-    name: 'Октябрьский'
-  }, {
-    id: 8,
-    name: 'Московский'
-  }, {
-    id: 9,
-    name: 'Фрунзенский'
-  }],
+  districts: [
+    'Центральный',
+    'Советский',
+    'Первомайский',
+    'Партизанский',
+    'Заводской',
+    'Ленинский',
+    'Октябрьский',
+    'Московский',
+    'Фрунзенский'
+  ],
   actions: {
     selectCommodity(value, item) {
-      this.set('summaryPrice', `Цена: ${item.price}$`);
-      this.set('model.item', value);
-      this.set('model.price', item.price);
+      this.set('summaryPrice', `Цена: ${item.get('price')}$`);
+      this.set('order.item', value);
+      this.set('order.price', item.get('price'));
     },
     selectDistrict(value) {
-      this.set('model.deliveryDistrict', value);
+      this.set('order.deliveryDistrict', value);
     },
     addOrder() {
       this.sendAction('actionToCall');
--- orders-table/app/routes/add.js
+++ orders-table/app/routes/add.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return {
-      order: this.get('store').createRecord('order')
+      order: this.get('store').createRecord('order'),
+      commodities: this.get('store').findAll('commodity')
     }
   },
   deactivate() {
