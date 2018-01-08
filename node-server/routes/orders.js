f20ef42f72bc989cd47bc5a74b7f6a095e0bed94
Alesya <alobach@lighthousenetwork.com>
2018-01-08 18:28:40 +0300

    Orders: Added orders removing functionality
    refs: HTDT-280

--- node-server/routes/orders.js
+++ node-server/routes/orders.js
@@ -1,6 +1,7 @@
 const express = require('express'),
       router = express.Router(),
-      fs = require('file-system');
+      fs = require('file-system'),
+	  shortId = require('shortid');
 
 router.get('/api/orders', (req, res) => {
     res.status(200).send(fs.readFileSync('./data/orders.json', 'utf8'));
@@ -10,13 +11,25 @@ router.post('/api/orders', (req, res) => {
     const data = JSON.parse(fs.readFileSync('./data/orders.json', 'utf8')).orders,
           order = req.body.order;
 
-    order.id = data.length + 1;
+    order.id = shortId.generate();
     order.dateAndTime = new Date().getTime();
     order.deliveryState = 'Ожидает доставки';
 
     data.push(order);
     fs.writeFileSync('./data/orders.json', JSON.stringify({orders : data}));
-    res.status(200).send({"order" : order});
+    res.status(200).send({'order' : order});
+});
+
+router.delete('/api/orders/:id', (req, res) => {
+	const data = JSON.parse(fs.readFileSync('./data/orders.json', 'utf8')).orders,
+	 	id = req.params.id;
+
+	const updatedData = data.filter((order) => {
+		return order.id !== id;
+	});
+
+	fs.writeFileSync('./data/orders.json', JSON.stringify({orders : updatedData}));
+	res.sendStatus(204);
 });
 
 module.exports = router;
