ad4bd4f4d0454af4f4eb0330221ea75325a67898
Alesya <alobach@lighthousenetwork.com>
2018-01-12 14:38:23 +0300

    Orders: Moved orders sorting to a component
    refs: HTDT-292

--- orders-table/app/components/orders-list.js
+++ orders-table/app/components/orders-list.js
@@ -1,6 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: 'personName',
+  sortDir: true,
+  sortedOrders: Ember.computed('filteredOrders.@each', 'sortBy', 'sortDir', function() {
+    const sortBy = this.get('sortBy');
+
+    return this.get('filteredOrders').sort((a, b) => {
+      let el1, el2;
+
+      el1 = this.get('sortDir') ? a.get(sortBy) : b.get(sortBy);
+      el2 = this.get('sortDir') ? b.get(sortBy) : a.get(sortBy);
+
+      if (el1 < el2) {
+        return -1;
+      }
+      if (el1 > el2) {
+        return 1;
+      }
+      return 0;
+    });
+  }),
   actions: {
     removeOrder(order) {
       const confirmRemoving = confirm('Вы уверены, что хотите удалить этот заказ?');
@@ -9,7 +29,8 @@ export default Ember.Component.extend({
       }
     },
     sortOrders(sortBy) {
-      this.sendAction('actionToCall', sortBy);
+      this.set('sortBy', sortBy);
+      this.set('sortDir', !this.get('sortDir'));
     }
   }
 });
